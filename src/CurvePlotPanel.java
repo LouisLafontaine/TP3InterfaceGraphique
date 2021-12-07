f77a6d3129b162e99f8312cfe54d3c99b1af943f
Louis Lafontaine <louishenri.lafontaine@gmail.com>
2021-12-07 19:23:31 +0100

    Moving background

--- src/CurvePlotPanel.java
+++ src/CurvePlotPanel.java
@@ -3,29 +3,37 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class CurvePlotPanel extends JPanel implements ActionListener {
 
-    private final Image background;
+    private final BufferedImage background;
+    private int bckgrdx = 0;
     private Curve curve;
     Timer timer;
 
     public CurvePlotPanel(){
         timer = new Timer(41,this);
         timer.start();
-        background = importImage("Backgrounds/classicBackground.png");
+        background = (BufferedImage) importImage("Backgrounds/classicBackground.png");
     }
 
     public void actionPerformed(ActionEvent e) {
         if(curve!=null){
             curve.displaceY(5);
+            bckgrdx += 5;
             repaint();
         }
     }
 
     public void paintComponent(Graphics g){
-        g.drawImage(background,0,0,getWidth(), getHeight(),null );
+        bckgrdx %= background.getWidth()-1; //-1 because 0 width is forbidden
+        Image imgLeft = background.getSubimage(bckgrdx, 0, background.getWidth()- bckgrdx, background.getHeight());
+        Image imgRight = background.getSubimage(0,0, bckgrdx,background.getHeight());
+       g.drawImage(imgLeft, 0,0,getWidth()- bckgrdx, getHeight(),null);
+       g.drawImage(imgRight,getWidth()- bckgrdx,0, bckgrdx, getHeight(),null);
+
         if(curve!=null){
             curve.draw(g);
         }
